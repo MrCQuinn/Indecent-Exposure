6ce0bb015b32074d07bce17b173d93ae39d48e0d
Portia Seater <portiaseater@Portias-MacBook-Pro.local>
2016-05-22 22:09:40 -0700

    Adding the start of a items class for use with power-ups

---
+++ game2/items.hpp
@@ -0,0 +1,44 @@
+//
+//  power_ups.hpp
+//  game2
+//
+//  Created by Portia Seater on 5/22/16.
+//  Copyright © 2016 SDL_Squad. All rights reserved.
+//
+
+#ifndef power_ups_hpp
+#define power_ups_hpp
+
+#include "imports.hpp"
+#include "sdl_setup.hpp"
+#include "sprite.hpp"
+#include <math.h>
+#include <vector>
+
+class Environment;
+
+class Items
+{
+public:
+    Items(SDL_Setup* passed_SDL_Setup, SDL_Texture* passed_image, int starting_x, int starting_y, int h, int w, Environment* passed_environment, int type);
+    Items() {}
+    ~Items();
+    
+    virtual void Update();
+    void Draw();
+    
+protected:
+    
+    Environment* environment;
+    SDL_Setup* sdl_setup;
+    Sprite* unit;
+    bool colliding;
+    
+    int prevX;
+    int prevY;
+    
+    
+    
+};
+
+#endif /* power_ups_hpp */

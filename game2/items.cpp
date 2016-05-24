6e5adf8abdf5400b34ce23a0cfcf6825f856afd6
Portia Seater <portiaseater@Portias-MacBook-Pro.local>
2016-05-23 22:52:58 -0700

    progress on items class

--- game2/items.cpp
+++ game2/items.cpp
@@ -11,18 +11,15 @@
 #include <math.h>
 
 
-Items::Items(SDL_Setup* passed_SDL_Setup, SDL_Texture* passed_image, int starting_x, int starting_y, int h, int w, Environment* passed_environment, int type) //Constructor
+Items::Items(SDL_Setup* passed_SDL_Setup, SDL_Texture* passed_image, int starting_x, int starting_y, int h, int w, Environment* passed_environment) //Constructor
 {
     environment = passed_environment;
     colliding = true;
     
     sdl_setup = passed_SDL_Setup;
     
-    unit = new Sprite(sdl_setup->GetRenderer(), passed_image, starting_x, starting_y, w, h);
+    unit = new Sprite(sdl_setup->GetRenderer(), passed_image, starting_x, starting_y, h, w);
     unit->SetOrigin((unit->GetWidth())/2, (unit->GetHeight())/2);
-    
-    prevX = starting_x;
-    prevY = starting_y;
 }
 
 
@@ -36,4 +33,18 @@ void Items::Draw()
     unit->Draw();
 }
 
+int Items::getItemX(){
+    return unit->GetX();
+}
 
+int Items::getItemY(){
+    return unit->GetY();
+}
+
+int Items::getItemW(){
+    return unit->GetWidth();
+}
+
+int Items::getItemH(){
+    return unit->GetHeight();
+}
